8a3146e7b46232394af925944f09ccaa3f5c3f98
David Quast <dquast@ryerson.ca>
2019-09-17 11:47:56 -0400

    Board component now can make a move

--- www/components/board.js
+++ www/components/board.js
@@ -1,61 +1,64 @@
+import { memory } from "../../pkg/othello_web_bg";
+import { Board, Piece }  from "../../pkg/othello_web";
 import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
 import Square from './square';
 
-/**
- * DQ:
- * Don't need to hard-code row values if keeping cells as single-dim array.
- * See changes to `render`.
- */
-// const rows = [11, 21, 31, 41, 51, 61, 71, 81];
-
 class GameBoard extends Component {
+
   constructor(props) {
     super(props);
-    this.cells = props.cells;
+    this.board = props.board;
+    this.state = {
+      cells: new Uint8Array(memory.buffer, this.board.cells(), 100),
+      turn: Piece.BLACK
+    }
   }
 
   /**
-   * DQ:
-   * Don't need to parse the cell array for ranges.  See change to `render`.
+   * Handle when one of the board squares are clicked.  For now, just makes the
+   * move for the player with no checks.
+   *
+   * @param {number} cellId The `id` of the clicked cell.
    */
-  // range(start, end) {
-  //   return (new Array(end -start + 1)).fill(undefined).map((_, i) => i + start);
-  // }
-
-  /**
-   * DQ:
-   * Don't need to render rows one by one any more. See changes to `render`.
-   */
-  // renderRow(i) {
-  //   return (
-  //     <div className="board-row" key={i}> 
-  //       { this.range(i, i+7).map(k => <Square key={k} id={k} value={this.state.cells[k]}/>) }
-  //     </div>
-  //   );
-  // }
+  onSquareClick(cellId) {
+    console.log(cellId);
+    this.board.make_move(cellId, this.state.turn);
+    this.setState({
+      cells: new Uint8Array(memory.buffer, this.board.cells(), 100),
+      turn: this.state.turn === Piece.BLACK ? Piece.WHITE : Piece.BLACK,
+    });
+  }
 
   /**
    * Render the board.
    */
   render() {
-    /**
-     * DQ:
-     * Need to convert the board to a regular JS array, otherwise the `map` call
-     * below just returns `0`s.
-     */
-    const cells = Array.from(this.cells);
+    const cells = Array.from(this.state.cells);
 
-    /**
-     * DQ:
-     * The cells of the board are stored as a single-dimension array, so just
-     * map that and rely on CSS-Grid to create the board.
-     */
     return (
       cells.map((cell, index) => {
-        return (<Square key={index} id={index} type={cell} />)
+        let cn = 'square';
+        if (cell === 3) {
+          cn += ' square--edge';
+        }
+
+        let pn = 'piece';
+        if (cell === 0) {
+          pn += ' piece--black';
+        }
+        if (cell === 1) {
+          pn += ' piece--white';
+        }
+
+        return (
+          <div className={cn} onClick={this.onSquareClick.bind(this, index)}>
+            <div className={pn} />
+          </div>
+        )
       })
-    );
+    )
   }
 }
 
-export default GameBoard;
+export default hot(module)(GameBoard);
